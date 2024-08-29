8ef8db2ff67ea7581ef31f1dd03ea5130c66de5d
Ignacio Baviera <bavieraignacio52@gmail.com>
2024-08-29 03:19:01 -0300

    otra

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 import pygame
 import random
 
+<<<<<<< Updated upstream
+=======
+class Entity:
+    
+    
+    def __init__(self, pos_x, pos_y):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+    
+    
+class Player(Entity):
+    
+    speed = 0,3
+    
+    def __init__(self ):
+        super().__init__(336, 236)
+        
+        
+    def play_now(self):
+        window.blit(self.pos_x ,self.pos_y)
+        
+        
+        
+        
+>>>>>>> Stashed changes
 
 pygame.init()
 
