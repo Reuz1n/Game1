5b3c5c1be72f8734331b3748c6ef2db90464a2f5
thesystew <nahufpb@gmail.com>
2024-08-29 03:34:22 -0300

    Update main.py
    Fixed

--- main.py
+++ main.py
@@ -2,40 +2,39 @@ import pygame
 import random 
 import sys
 
-<<<<<<< Updated upstream
-=======
 class Entity:
-    
-    
+
+
     def __init__(self, pos_x, pos_y, health):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.health = health
-    
-    
+
+
 class Player(Entity):
-    
+
     speed = 0,3
-    
-    def __init__(self ):
-        super().__init__(336, 236, 3)
-        
-        
+    player_icon = pygame.image.load("images\player.png")
+
+    def __init__(self):
+        super().__init__(352, 515, 3)
+
+
     def play_now(self):
-        window.blit((self.pos_x ,self.pos_y))
-        
-        
-        
-        
+        window.blit(self.player_icon, (self.pos_x ,self.pos_y))
+
+
+
+
 
 pygame.init()
 window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("NAVECITAS LOCURA")
-icon = pygame.image.load("Game1\Images\icon.png")
+icon = pygame.image.load("images\icon.png")
 pygame.display.set_icon(icon)
-background = pygame.image.load("Game1\Images\wallpaper.jpg")
+background = pygame.image.load("images\wallpaper.jpg")
 
-    
+
 player1 = Player()
 
 
@@ -47,5 +46,5 @@ while True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        
+
     pygame.display.update()
