ce2f6bbe67cd84af8ab89375b4c41b32ac43e154
thesystew <nahufpb@gmail.com>
2024-08-30 03:11:02 -0300

    ez
    working

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import sys
+import math
 
 class Entity:
 
@@ -14,7 +15,7 @@ class Player(Entity):
   
   # Variables de clase
   speed = 0
-  player_icon = pygame.image.load("Game1\images\player.png")
+  player_icon = pygame.image.load("images\player.png")
 
   def __init__(self):
     super().__init__(352, 515)
@@ -27,15 +28,16 @@ class Player(Entity):
     self.speed = value
   
   def cast_bullet(self):
-    bullet = Bullet(self.pos_x, self.pos_y)
+    bullet = Bullet(self.pos_x + 16, self.pos_y - 16)
     bullet.shoot()
+    return bullet
     
 
 
 class Enemy(Entity): 
 
   # Variables de clase
-  icon = pygame.image.load("Game1\images\enemy.png")
+  icon = pygame.image.load("images\enemy.png")
   enemies = []  
 
   def __init__(self, pos_x, pos_y, direction):
@@ -57,36 +59,57 @@ class Enemy(Entity):
 
 class Bullet(Entity):
   
-  speed = 10
-  icon = pygame.image.load("Game1\images\\bullet.png")
-  
+  is_alive = True
+  speed = 15
+  icon = pygame.image.load("images\\bullet.png")
   
   def __init__(self, pos_x, pos_y):
     super().__init__(pos_x, pos_y)
     
   def shoot(self):
     self.pos_y -= self.speed
-    window.blit(self.icon, (self.pos_x ,self.pos_y))
+    window.blit(self.icon, (self.pos_x, self.pos_y))
+    self.out_of_screen()
+
+  def colision(self, enemy_list):
+    for enemy in enemy_list:
+      if (math.sqrt((enemy.pos_x - self.pos_x)**2 + (enemy.pos_y - self.pos_y)**2) < 32):
+        self.is_alive = False
+        enemy_list.remove(enemy)
+        del enemy
+        break
+
+  def out_of_screen(self):
+    if self.pos_y <= 0:
+      self.is_alive = False
+
+  def get_state(self):
+    return self.is_alive
   
 
-# Inicio de juego
+# Inicio de juego 
 
 pygame.init()
 
 # Variables del juego
 window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("NAVECITAS LOCURA")
-icon = pygame.image.load("Game1\images\icon.png")
+icon = pygame.image.load("images\icon.png")
 pygame.display.set_icon(icon)
-background = pygame.image.load("Game1\images\wallpaper3.jpg")
+background = pygame.image.load("images\wallpaper3.jpg")
 FPS = 60 # Constante FPS
 
 # Variables globales 
 enemies = [] 
 enemies_max = 10
 cantity = 5
+bullets = []
 main_player = Player()
 
+# Crea y guarda enemigos en la lista si es que no llego al maximo 
+while len(enemies) < enemies_max:
+  enemy = Enemy(random.randint(0, 736), random.randint(50, 200), random.choice(["left", "right"]))
+  enemies.append(enemy) 
 
 #Loop main
 while True:
@@ -96,11 +119,6 @@ while True:
   
   # Reescribe el personaje
   main_player.move_player()
-
-  # Crea y guarda enemigos en la lista si es que no llego al maximo 
-  if len(enemies) < enemies_max:
-        enemy = Enemy(random.randint(0, 736), random.randint(50, 200), random.choice(["left", "right"]))
-        enemies.append(enemy) 
 
   # Mueve y genera todos los enemigos
   for enemy in enemies:
@@ -113,9 +131,10 @@ while True:
     
     # Logica de movimiento
     if event.type == pygame.KEYDOWN:
-      #disparo y colision
-      if event.key == pygame.K_SPACE:
-        pass
+      # Disparo
+      if event.key == pygame.K_SPACE and len(bullets) < 1:
+        new_bullet = main_player.cast_bullet()
+        bullets.append(new_bullet)
       if event.key == pygame.K_LEFT:
         main_player.set_speed(-7)
       if event.key == pygame.K_RIGHT:
@@ -125,7 +144,6 @@ while True:
         main_player.set_speed(0)
       if event.key == pygame.K_RIGHT and main_player.speed >= 7:
         main_player.set_speed(0)
-  
 
     
   # Evitar que salga de pantalla
@@ -133,7 +151,18 @@ while True:
     main_player.pos_x = 0
   elif main_player.pos_x >= 736:
     main_player.pos_x = 736
-  
+
+
+  if len(bullets) > 0:
+    for bullet in bullets:
+      if bullet.get_state():
+        bullet.shoot()
+        bullet.colision(enemies)
+      else:
+        bullets.remove(bullet)
+        del bullet
+        
+
   # Actualiza el juego
   pygame.display.flip()
 
