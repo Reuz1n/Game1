13a9ccfc6e5ca0b02666a65f48d03cdb90b7d42a
Ignacio Baviera <bavieraignacio52@gmail.com>
2024-08-29 22:13:37 -0300

    mejor que antes

--- main.py
+++ main.py
@@ -1,47 +1,57 @@
 import pygame
-import random 
+import random
 import sys
 
 class Entity:
 
 
-    def __init__(self, pos_x, pos_y, health=None):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.health = health
+  def __init__(self, pos_x, pos_y, health=None):
+    self.pos_x = pos_x
+    self.pos_y = pos_y
+    self.health = health
 
 
 class Player(Entity):
 
-    speed = 0
+  speed = 0
 
-    player_icon = pygame.image.load("Game1\images\player.png")
+  player_icon = pygame.image.load("Game1\images\player.png")
 
-    def __init__(self):
-        super().__init__(352, 515)
+  def __init__(self):
+    super().__init__(352, 515)
 
 
-    def move_player(self):
-        self.pos_x += self.speed
-        window.blit(self.player_icon, (self.pos_x ,self.pos_y))
+  def move_player(self):
+    self.pos_x += self.speed
+    window.blit(self.player_icon, (self.pos_x ,self.pos_y))
 
-    
-    def set_speed(self, value):
-        self.speed = value
+  
+  def set_speed(self, value):
+    self.speed = value
 
 
 class Enemy(Entity): 
-    enemy_icon = pygame.image.load("Game1\images\enemy.png")
-    enemies = []
-    def __init__(self, pos_x, pos_y ):
-        super().__init__(pos_x, pos_y)
-    
-    def move_enemy(self):
-        self.pos_x = random.randint(0, 800)
-        self.pos_y = random.randint(500, 600)
-        window.blit(self.enemy_icon, (self.pos_x, self.pos_y)) 
-    
+  icon = pygame.image.load("Game1\images\enemy.png")
+  enemies = []  
 
+  def __init__(self, pos_x, pos_y, direction):
+    super().__init__(pos_x, pos_y)
+    self.direction = direction
+    
+  def move_enemy(self):
+    if self.direction == "right":
+        self.pos_x += 1 #borde derechp
+        if self.pos_x >= 736:  
+            self.direction = "left"
+            self.pos_y += 10  # baja 10px abajo
+    elif self.direction == "left":
+        self.pos_x -= 1      #borde izquierdo
+        if self.pos_x <= 0:
+            self.direction = "right"
+            self.pos_y += 10
+        window.blit(self.icon, (self.pos_x, self.pos_y))
+
+enemies = [] 
 
 pygame.init()
 window = pygame.display.set_mode((800, 600))
@@ -50,46 +60,46 @@ icon = pygame.image.load("Game1\images\icon.png")
 pygame.display.set_icon(icon)
 background = pygame.image.load("Game1\images\wallpaper.jpg")
 
+enemies_max = 10
 cantity = 5
 main_player = Player()
-enemies = Enemy(random.randint(0, 800), random.randint(500, 600))
+
 
 while True:
-    window.blit(background, (0, 0))
-    
-    main_player.move_player()
-    
-    #crea enemigos por la cantidad en "Cantity"
-    for _ in range(cantity):  
-        enemy = Enemy(random.randint(0, 800), random.randint(500, 600))
-        Enemy.enemies.append(enemy)  
+  window.blit(background, (0, 0))
+  
+  main_player.move_player()
 
-   # mueve enemigos por cada uno
-    for enemy in Enemy.enemies:
-        enemy.move_enemy()  
-        window.blit(enemy.enemy_icon, (enemy.pos_x, enemy.pos_y))
-    
-    for event in pygame.event.get():
+  # Crea y guarda enemigos en la lista si es que no llego al maximo 
+  if len(enemies) < enemies_max:
+        enemy = Enemy(random.randint(0, 736), random.randint(50, 200), random.choice(["left", "right"]))
+        enemies.append(enemy) 
 
-        if event.type == pygame.QUIT: sys.exit()
+ # mueve y genera todos los enemigos
+  for enemy in enemies:
+    enemy.move_enemy()  
+    window.blit(enemy.icon, (enemy.pos_x, enemy.pos_y))  
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                main_player.set_speed(-0.3)
-            if event.key == pygame.K_RIGHT:
-                main_player.set_speed(0.3)
+  for event in pygame.event.get():
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT and main_player.speed == -0.3:
-                main_player.set_speed(0)
-            if event.key == pygame.K_RIGHT and main_player.speed == 0.3:
-                main_player.set_speed(0)
-                
-    # no sale de pantalla
-    if main_player.pos_x <= 0:
-        main_player.pos_x = 0
-    elif main_player.pos_x >= 736:
-        main_player.pos_x = 736
-      
+    if event.type == pygame.QUIT: sys.exit()
 
-    pygame.display.update()
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        main_player.set_speed(-0.3)
+      if event.key == pygame.K_RIGHT:
+        main_player.set_speed(0.3)
+
+    if event.type == pygame.KEYUP:
+      if event.key == pygame.K_LEFT and main_player.speed == -0.3:
+        main_player.set_speed(0)
+      if event.key == pygame.K_RIGHT and main_player.speed == 0.3:
+        main_player.set_speed(0)
+  
+  # no sale de pantalla
+  if main_player.pos_x <= 0:
+    main_player.pos_x = 0
+  elif main_player.pos_x >= 736:
+    main_player.pos_x = 736
+  
+  pygame.display.update()
